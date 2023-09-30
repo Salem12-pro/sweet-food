09dcb5acc395ebeb126ecdfc69698d72b453fdf3
salem <muhammedsalem992@gmail.com>
2023-09-30 09:17:44 -0700

    salem

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
  var hamburger = document.querySelector(".hamburger")
-
+ var mobilelinks = document.querySelector(".mobileview2")
+ var backdrop = document.querySelector(".backdrop")
 
 
  function dosomething() {
     hamburger.classList.toggle("showburger")
+    mobilelinks.classList.toggle("showview")
+    backdrop.classList.toggle("showview")
  }
